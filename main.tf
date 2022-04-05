38deb45d486a029133976ae64740f34ab3386153
Majid Khan <67179406+Majid-khan191@users.noreply.github.com>
2022-04-06 02:42:32 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
     region = "ap-south-1"
-    access_key = "AKIAZVAVBUYHETHTKAUC"
-    secret_key = "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"
+    access_key = "AKIAZVAVBUYHETHTfmikv"
+    secret_key = "G3fQrXvjZcwjpEay1yiLIxDTsdUUdAjgncfni"
 }
 
 
